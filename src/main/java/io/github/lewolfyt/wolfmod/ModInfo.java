0ed7eba60c2253ff6f1f60efc399d5973255e619
Sid <68089579+BasicCorruption@users.noreply.github.com>
2021-06-09 12:31:25 -0400

    Create ModInfo.java

---
+++ src/main/java/io/github/lewolfyt/wolfmod/ModInfo.java
@@ -0,0 +1,11 @@
+package io.github.lewolfyt.wolfmod;
+
+class ModInfo {
+    public static String MOD_NAME = "WolfMod";
+    public static String MOD_ID = "wolfmod";
+    public static String MOD_VERSION = "1.0.0-alpha";
+
+    public static String MINECRAFT_VERSION = "1.17";
+
+    public static String FABRIC_VERSION = "0.11.3";
+}
